2e197d5e994797603057b8050d127f5d1df45aff
wesley willekes <wesleywillekes@MacBook-Pro-van-wesley.local>
2017-07-12 19:21:49 +0200

    Added mobile support to description box
    Mobile users will now be able to drag to/from the left of the screen to toggle the description box.

--- assets/description.js
+++ assets/description.js
@@ -1,3 +1,4 @@
+// For desktop machines, pressing the tab key will trigger the description box
 document.addEventListener("keydown", function(event){
 	if(event.keyCode == 9){
 		var div = $("#page-desc");
@@ -12,6 +13,48 @@ document.addEventListener("keydown", function(event){
 		}
 
 		event.preventDefault();
+	}
+});
+
+// For mobile devices, sliding from/to the left will trigger the description box
+
+var orgX; // original x pos of the touch
+var orgY; // original y pos of the touch
+
+
+document.addEventListener("touchstart", function(event){
+	if(event.touches.length == 1){
+		orgX = event.touches[0].pageX;
+		orgY = event.touches[0].pageY;
+	}else{ // if there are multiple touches, cancel the drag
+		orgX = null;
+		orgY = null;
+	}
+});
+
+document.addEventListener("touchmove", function(event){
+	if(event.touches.length == 1 && (orgX || orgX == 0) && (orgY || orgY == 0)){
+		if(orgX <= window.innerWidth / 4){
+			event.preventDefault();
+		}
+	}
+}, {"passive": false});
+
+document.addEventListener("touchend", function(event){
+	if(event.touches.length == 0 && (orgX || orgX == 0) && (orgY || orgY == 0)){ // validate the situation
+		if(orgX <= window.innerWidth / 4){ // make sure the touch started on the left 1/4 of the screen
+			var div = $("#page-desc");
+			var touch = event.changedTouches[0];
+
+			if(!(div.hasClass("fade-in") || div.hasClass("fade-out"))){ // make sure there is no animation in progress
+				if(div.hasClass("hidden") && touch.pageX - orgX > 0){
+					div.addClass("fade-in");
+					div.removeClass("hidden");
+				}else if(!div.hasClass("hidden") && touch.pageX - orgX < 0){
+					div.addClass("fade-out");
+				}
+			}
+		}
 	}
 });
 
